2afc32db2d5bc94837c8276c8126d5eb63d97dd2
Rafael dos Santos <rafaels1997@LIVE.COM>
2022-06-30 00:56:52 -0300

    listagem funcionando

---
+++ js/listagem.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    listar();
+});
+
+function listar(){
+
+    let itensTabela = buscarDeLocalStorage("itensTabela");
+    itensTabela = transformaJsonEmObjeto(itensTabela);
+    itensTabela.forEach(inserirLinha);
+    
+}
+
+function inserirLinha(item, index){
+
+    let dataFabricacao = new Date(item.dataFabricacao);
+    let dataValidade = new Date(item.dataValidade);
+    dataFabricacao = dataFabricacao.getDate() + "/" + (dataFabricacao.getMonth() + 1) + "/" + dataFabricacao.getFullYear();
+    dataValidade = dataValidade.getDate() + "/" + (dataValidade.getMonth() + 1) + "/" + dataValidade.getFullYear();
+    debugger;
+
+    let html = `<tr>
+    <td></td>
+    <td>${index}</td>
+    <td>${item.nome}</td>
+    <td>${item.quantidade}</td>
+    <td>${item.preco}</td>
+    <td>${dataFabricacao}</td>
+    <td>${dataValidade}</td>
+    </tr>`;
+
+    $("#tbody").append(html);
+}
