78af89179837588df33e52e027a10dddc61b86ad
Rafael dos Santos <rafaels1997@LIVE.COM>
2022-06-29 22:06:25 -0300

    salvando em local storage

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -25,29 +25,6 @@ function salvar(){
     }
 
     itensTabela.push(item);
-    alert(transformarEmJson(itensTabela));
     itensTabela = transformarEmJson(itensTabela);
     salvarEmLocalStorage(itensTabela);
-}
-
-function transformarEmJson(objeto){
-    let retorno = JSON.stringify(objeto);
-
-    return retorno;
-}
-
-function salvarEmLocalStorage(json){
-    localStorage.setItem("itensTabela", json);
-}
-
-function buscarDeLocalStorage(){
-    let retorno = localStorage.getItem("itensTabela");
-
-    return retorno;
-}
-
-function transformaJsonEmObjeto(json){
-    let retorno = JSON.parse(json);
-
-    return retorno;
 }
