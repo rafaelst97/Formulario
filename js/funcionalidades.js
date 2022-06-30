aa28b855f27fac2bb5f4a52f5b241c9e3d8c9845
Rafael dos Santos <rafaels1997@LIVE.COM>
2022-06-29 22:04:57 -0300

    criação de arquivo com utilidades

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -8,5 +8,46 @@ function salvar(){
     let dataValidade = new Date($("#validade").val());
     let dataFabricacao = new Date($("#fabricacao").val());
 
-    alert(dataFabricacao);
+    let item ={
+        nome: nome,
+        unidade: unidade,
+        quantidade: quantidade,
+        preco: preco,
+        perecivel: perecivel,
+        dataFabricacao: dataFabricacao
+    };
+
+    let itensTabela = buscarDeLocalStorage("itensTabela");
+    itensTabela = transformaJsonEmObjeto(itensTabela);
+    
+    if (itensTabela == null){
+        itensTabela = [];
+    }
+
+    itensTabela.push(item);
+    alert(transformarEmJson(itensTabela));
+    itensTabela = transformarEmJson(itensTabela);
+    salvarEmLocalStorage(itensTabela);
+}
+
+function transformarEmJson(objeto){
+    let retorno = JSON.stringify(objeto);
+
+    return retorno;
+}
+
+function salvarEmLocalStorage(json){
+    localStorage.setItem("itensTabela", json);
+}
+
+function buscarDeLocalStorage(){
+    let retorno = localStorage.getItem("itensTabela");
+
+    return retorno;
+}
+
+function transformaJsonEmObjeto(json){
+    let retorno = JSON.parse(json);
+
+    return retorno;
 }
